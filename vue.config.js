90292b3ea192f6b1cefb1ac0a9d5e8f0072ddcf6
Semo4ka <mr.vasilchenoko@mail.ru>
2024-06-20 06:38:49 +0300

    123

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/techno-service-storage/'
-    : '/'
 })
+
+module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+    ? ''
+    : ''
+}
